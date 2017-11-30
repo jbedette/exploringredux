efb609d130ea7d0d7a27a5b5f9300bb5bcd4a523
John Bedette <jbedette@yahoo.com>
2017-11-30 17:23:47 -0500

    fleshing out component structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
 // create a new component this component should produce some html
 
 // Take this component's generated html and put it on the page
+
+const App  = function(){
+    return <div>Fart</div>;
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'))
